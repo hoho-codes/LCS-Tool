82f5656d2ad4d2004409e4d59f77f75013173d3e
hoho-codes <60325523+hoho-codes@users.noreply.github.com>
2021-12-25 14:50:28 +0530

    Update DoubleGyreDerivative.m

--- double_gyre_vel_vort/DoubleGyreDerivative.m
+++ double_gyre_vel_vort/DoubleGyreDerivative.m
@@ -1,5 +1,9 @@
+
+%% Function that returns velocity values of the Double Gyre stream function taking
+%% the parameters eps,w,A as inputs
+
 function dxdt = DoubleGyreDerivative(t,x,eps,w,A)
-  
+
   f = @(t,z) (eps*sin(w*t)*z*z+(1-2*eps*sin(w*t))*z);
   dfdx = @(t,a) (2*eps*a*sin(w*t)+(1-2*eps*sin(w*t)));
 
