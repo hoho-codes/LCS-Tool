3410db89078911b9bad02c9f03f77a208bc9908d
hoho-codes <60325523+hoho-codes@users.noreply.github.com>
2021-12-25 11:54:09 +0530

    Update DGFTLEDATAGENERATOR.m

--- double_gyre_lcs/DGFTLEDATAGENERATOR.m
+++ double_gyre_lcs/DGFTLEDATAGENERATOR.m
@@ -1,6 +1,6 @@
-%% Script to generate '.mat' file storing FTLE Field values at 100 timeshots from 0 to 10 seconds at 0.1s intervals 
-end_time = 10
-time_shot_interval = 0.1
+%% Script to generate '.mat' file storing FTLE Field values at 101 timeshots from 0 to 10 seconds at 0.1s intervals 
+end_time = 10;
+time_shot_interval = 0.1;
 
 % Double gyre parameters
 epsilon = 0.25;
@@ -18,33 +18,35 @@ incompressible = true;
 % Cauchy-Green strain
 cgStrainOdeSolverOptions = odeset('relTol',1e-5);
 
-ftleValuesf=double.empty;
-ftleValuesb=double.empty;
-for j=0:endtime*time_shot_interval
+ftleValuesf=zeros(end_time/time_shot_interval+1,resolutionX*resolutionY);
+ftleValuesb=zeros(end_time/time_shot_interval+1,resolutionX*resolutionY);
+ft = zeros(1,resolutionX*resolutionY);
+
+for j=0:end_time/time_shot_interval
     
     % Cauchy-Green strain eigenvalues for forward time FTLE field
     timespan = [j*time_shot_interval,end_time+j*time_shot_interval];
     [~,cgEigenvalue] = eig_cgStrain(lDerivative,domain,resolution,timespan,'incompressible',incompressible,'odeSolverOptions',cgStrainOdeSolverOptions);
     
     % Generate forward time FTLE field values at specified initial time
-    maxeg = max(cgEigenvalue');
+    maxeg = max(cgEigenvalue,[],2);
     for i=1:resolutionX*resolutionY
         temp = maxeg(i);
         ft(i) = ftle(temp,abs(abs(timespan(2)-timespan(1))));
     end
-    ftleValuesf = [ftleValuesf;ft];
+    ftleValuesf(j+1,:) = ft;
     
     % Cauchy-Green strain eigenvalues for backward time FTLE field
     timespan = [j*time_shot_interval,-end_time+j*time_shot_interval];
     [~,cgEigenvalue] = eig_cgStrain(lDerivative,domain,resolution,timespan,'incompressible',incompressible,'odeSolverOptions',cgStrainOdeSolverOptions);
     
     % Generate backward time FTLE field values at specified initial time
-    maxeg = max(cgEigenvalue');
+    maxeg = max(cgEigenvalue,[],2);
     for i=1:resolutionX*resolutionY
         temp = maxeg(i);
         ft(i) = ftle(temp,abs(abs(timespan(2)-timespan(1))));
     end
-    ftleValuesb = [ftleValuesb;ft];
-
+    ftleValuesb(j+1,:) = ft;
+    
 end
 save('double_gyre_ftle_values_0to10s.mat','ftleValuesf','ftleValuesb','resolution');
